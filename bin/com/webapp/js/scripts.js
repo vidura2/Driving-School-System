c38ac249937628839d5220759d1535319e192d9f
vidura2 <shanevidura@gmail.com>
2025-03-12 23:18:03 +0530

    deleted the bin

--- bin/com/webapp/js/scripts.js
+++
@@ -1,8 +0,0 @@
-function validateForm() {
-    let email = document.querySelector('input[name="email"]');
-    if (email && !email.value.includes('@')) {
-        alert('Please enter a valid email');
-        return false;
-    }
-    return true;
-}
